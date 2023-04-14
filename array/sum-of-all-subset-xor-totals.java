56fda270a3e3473ca92ae5cc74bded79f66dac99
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2023-04-14 05:25:14 -0300

    sum-of-all-subset-xor-totals

---
+++ array/sum-of-all-subset-xor-totals.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int result = 0;
+        int n = nums.length;
+        for (int i = 0; i < (1 << n); i++) {
+            int xor = 0;
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) != 0) {
+                    xor ^= nums[j];
+                }
+            }
+            result += xor;
+        }
+        return result;
+    }
+}
