d6f880af85b368362573c7508481549e7801ea66
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2023-04-27 05:19:35 -0300

    minimum-operations-to-make-the-array-increasing

---
+++ array/minimum-operations-to-make-the-array-increasing.java
@@ -0,0 +1,12 @@
+class Solution {
+ public static int minOperations(int[] nums) {
+    int count = 0;
+    for (int i = 1; i < nums.length; i++) {
+        if (nums[i] <= nums[i - 1]) {
+            count += nums[i - 1] - nums[i] + 1;
+            nums[i] = nums[i - 1] + 1;
+        }
+    }
+    return count;
+    }
+}
