99478c6832746b071c4efbe85dc71466ba9d9dcd
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2023-05-20 12:37:06 -0300

    Update find-target-indices-after-sorting-array.java

--- array/find-target-indices-after-sorting-array.java
+++ array/find-target-indices-after-sorting-array.java
@@ -2,14 +2,10 @@ class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
         Arrays.sort(nums);
         int count = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == target){
-                count++;
-            }
-        }
         List<Integer> ans = new ArrayList<>();
         for(int i = 0; i < nums.length; i++){
             if(nums[i] == target){
+                count++;
                 ans.add(i); 
             }
         }
